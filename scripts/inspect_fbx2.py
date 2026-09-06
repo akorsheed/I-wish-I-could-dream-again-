9328c635b1f0765b751b13e802219665d6f17faf
Adam Korsheed <adamkorsheed@gmail.com>
2026-09-05 21:41:04 -0400

    Clean Up

--- scripts/inspect_fbx2.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-path_fbx = r"C:\Users\start\Downloads\Graves_v3.fbx"
-bpy.ops.wm.read_factory_settings(use_empty=True)
-bpy.ops.import_scene.fbx(filepath=path_fbx)
-print("Objects in Graves_v3.fbx:", [o.name for o in bpy.data.objects])
-print("Meshes in Graves_v3.fbx:", [o.name for o in bpy.data.objects if o.type == 'MESH'])
-print("Actions in Graves_v3.fbx:", [a.name for a in bpy.data.actions])
--- scripts/test_fbx.gd
+++
@@ -1,7 +0,0 @@
-extends SceneTree
-
-func _init():
-	print("Checking ResourceLoader for fbx...")
-	var res = ResourceLoader.load("res://player/animations/idle.fbx")
-	print("Loaded fbx directly? ", res)
-	quit(0)
